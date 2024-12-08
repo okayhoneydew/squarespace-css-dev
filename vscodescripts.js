5571dff961fbcff429fb9ddec1e48b1fad56621b
okayhoneydew <spozywio1032@gmail.com>
2024-12-08 13:30:20 -0600

    Auto-commit: 2024-12-08 13:30:20

--- vscodescripts.js
+++ vscodescripts.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
   bannerImage.src = "https://images.squarespace-cdn.com/content/64a3d7a298b6160bf3ff7eed/937a8c56-415c-4a09-805a-4600d97be078/Private+banner+%28JPEG%29.jpg?content-type=image%2Fjpeg";
   bannerImage.alt = "Category Banner";
   bannerImage.style.width = "100%"; // Makes the banner full-width
-  bannerImage.style.marginBottom = "20px"; // Adds space below the banner
+  bannerImage.style.marginBottom = "20px"; // Adds space below the banne
 
   // Insert the image before the <h2>
   const h2Element = parentContainer.querySelector(".nested-category-title");
