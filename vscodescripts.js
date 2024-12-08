f5659b22accf58d88027c0beee4a5544e17d3b8b
okayhoneydew <spozywio1032@gmail.com>
2024-12-08 14:03:00 -0600

    Auto-commit: 2024-12-08 14:03:00

--- vscodescripts.js
+++ vscodescripts.js
@@ -1,8 +1,8 @@
     document.addEventListener("DOMContentLoaded", function () {
         const collectionLinks = document.querySelectorAll('.category-link'); // Links to collections
-        const bannerSection = document.querySelector('.fluid-engine fe-6755f5cf942dc92847289cfb'); // The main section
+        const bannerSection = document.querySelector('section[data-section-id="6755f5cfba217e7b47f3b6c0"]'); // The main section
 
-        // Define collection-specific image
+        // Define collection-specific images
         const collectionImages = {
             "collections": "url('https://images.squarespace-cdn.com/content/64a3d7a298b6160bf3ff7eed/3d7e0dd6-7a26-4205-8d03-df363bf829ec/cameron-highlands-tea-plantations-4-1195899.jpg?content-type=image%2Fjpeg')", // Default banner for the main collection page
             "private-collection": "url('https://images.squarespace-cdn.com/content/64a3d7a298b6160bf3ff7eed/937a8c56-415c-4a09-805a-4600d97be078/Private+banner+%28JPEG%29.jpg?content-type=image%2Fjpeg')",
