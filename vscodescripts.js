b208abc7526f40faf171617636ddd146fdbaeb41
okayhoneydew <spozywio1032@gmail.com>
2024-12-08 14:12:37 -0600

    Auto-commit: 2024-12-08 14:12:37

--- vscodescripts.js
+++ vscodescripts.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const collectionLinks = document.querySelectorAll('.category-link'); 
     
     // Select the section by its data-section-id
-    const bannerSection = document.querySelector('section[data-section-id="6755f5cfba217e7b47f3b6c0"]'); 
+    const bannerSection = document.querySelector('fe-6755f5cf942dc92847289cfb"]'); 
     
     // Check if the section is selected correctly
     if (!bannerSection) {
